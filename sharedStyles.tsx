a99d4d67cd4bee17cdda01beef41b044de384110
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-01-12 10:54:44 +0200

    added new colors

--- sharedStyles.tsx
+++ sharedStyles.tsx
@@ -1,9 +1,14 @@
 import { TextStyle, ViewStyle } from 'react-native';
 
 export const darkGreen = '#214f4f';
-export const darkGreen1 = '#308585';
-export const darkGreen2 = '#1e5f5f';
-export const darkGreen3 = '#154444';
+export const darkGreen600 = '#3a6b6b';
+export const darkGreen500 = '#568989';
+export const darkGreen400 = '#74a5a5';
+export const darkGreen300 = '#97bfbf';
+export const darkGreen200 = '#add1d1';
+export const darkGreen100 = '#c5e0e0';
+export const darkGreen000 = '#eff7f7';
+
 export const lightGreen = '#38abab';
 export const customGreen = '#235323';
 
