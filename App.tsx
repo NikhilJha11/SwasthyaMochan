c21417170327d6f22a5c7d64a5dae8be663c1c0b
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-01-12 09:56:23 +0200

    added default theme

--- App.tsx
+++ App.tsx
@@ -4,7 +4,16 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+import { colors } from './colors';
+
+const theme = {
+  ...DefaultTheme,
+  colors: colors, // Copy it from the color codes scheme and then use it here
+};
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +23,7 @@ export default function App() {
     return null;
   } else {
     return (
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <SafeAreaProvider>
           <Navigation colorScheme={colorScheme} />
           <StatusBar />
