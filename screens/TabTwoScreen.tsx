b1eadab1bf90d2e43e3f303fc6d940c141f83afc
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-01-14 10:07:51 +0200

    adjusted cta components

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -20,8 +20,10 @@ import {
   useTheme,
 } from 'react-native-paper';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import CTABig from '../components/CTABig';
 import DoctorBanner from '../components/DoctorBanner';
 import NewsItem from '../components/NewsItem';
+import OneHealAppBar from '../components/OneHealAppBar';
 import OneHealSafeArea from '../components/OneHealSafeArea';
 
 import { View } from '../components/Themed';
@@ -55,34 +57,7 @@ export default function TabTwoScreen() {
   return (
     <OneHealSafeArea statusBar='light'>
       <View style={styles.container}>
-        <View
-          style={[
-            styles.welcomer,
-            { paddingTop: insets.top * 1.2, marginTop: -insets.top },
-          ]}
-        >
-          <Avatar.Image source={require('../assets/images/avatar.jpg')} />
-
-          <View style={{ backgroundColor: darkGreen }}>
-            <Image
-              source={require('../assets/images/logo-reverted.png')}
-              style={{ width: 150, height: 50 }}
-              resizeMode='contain'
-            />
-          </View>
-          <Menu
-            visible={visible}
-            onDismiss={closeMenu}
-            anchor={
-              <Button onPress={openMenu} textColor='#fff'>
-                <Ionicons name='earth' /> EN
-              </Button>
-            }
-          >
-            <Menu.Item onPress={() => {}} title='Item 1' />
-            <Menu.Item onPress={() => {}} title='Item 2' />
-          </Menu>
-        </View>
+        <OneHealAppBar />
         <ScrollView style={{ flex: 1 }}>
           <View style={styles.news}>
             <FlatList
@@ -95,18 +70,11 @@ export default function TabTwoScreen() {
               snapToInterval={Dimensions.get('window').width - 100}
             />
           </View>
-          <View style={styles.bookAppointment}>
-            <TouchableOpacity style={styles.bookAppointmentButton}>
-              <Feather
-                name='calendar'
-                color='rgba(255,255,255,0.8)'
-                size={24}
-              />
-              <Text variant='headlineSmall' style={styles.bookAppointmentText}>
-                Book Appointment
-              </Text>
-            </TouchableOpacity>
-          </View>
+          <CTABig
+            icon={'calendar'}
+            text='Book Appointment'
+            style={styles.ctaBig}
+          />
           <DoctorBanner
             doctor={{
               doctorName: 'Dr. Kyle Bush',
@@ -143,23 +111,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'transparent',
   },
-  welcomer: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: darkGreen,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 2.65,
-    elevation: 5,
-    zIndex: 3,
-  },
+
   text: { paddingLeft: 10 },
   news: {
     height: 200,
@@ -170,32 +122,5 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 10,
     marginTop: 10,
   },
-  bookAppointment: {
-    backgroundColor: darkGreen,
-
-    marginHorizontal: 10,
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginVertical: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 0.22,
-    shadowRadius: 2.22,
-    elevation: 3,
-    alignItems: 'center',
-  },
-  bookAppointmentText: {
-    color: 'rgba(255,255,255,0.8)',
-    fontWeight: '600',
-    marginLeft: 12,
-  },
-  bookAppointmentButton: {
-    backgroundColor: darkGreen,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
+  ctaBig: { marginHorizontal: 10 },
 });
