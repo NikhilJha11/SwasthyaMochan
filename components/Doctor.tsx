327af74fcc3255e82139aa6e37805ed935bae610
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-01-15 11:51:21 +0200

    changed component

--- components/Doctor.tsx
+++ components/Doctor.tsx
@@ -3,21 +3,41 @@ import {
   StyleSheet,
   TouchableOpacity,
   View,
+  ViewStyle,
 } from 'react-native';
 import React from 'react';
 import { Avatar, Text } from 'react-native-paper';
 import { darkGreen, darkGreen050 } from '../sharedStyles';
+import { useNavigation } from '@react-navigation/native';
 
 type Props = {
   image: string;
   name: string;
   department: string;
   location: string;
+  id?: number;
+  styles?: ViewStyle;
 };
 
 const Doctor = (props: Props) => {
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity style={styles.doctor}>
+    <TouchableOpacity
+      style={[styles.doctor, props.styles]}
+      onPress={() =>
+        navigation.navigate('AppointmentStack', {
+          screen: 'AppointmentScreen',
+          params: {
+            department: props.department,
+            image: props.image,
+            location: props.location,
+            name: props.name,
+            id: props.id,
+          },
+        })
+      }
+    >
       <Avatar.Image source={props.image as ImageSourcePropType} size={56} />
       <View style={styles.doctorTexts}>
         <Text
