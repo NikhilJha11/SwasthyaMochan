9c4d9c417cd17b1da45187b972182c5b6fbe6470
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-01-15 11:50:10 +0200

    added id

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -148,6 +148,7 @@ export default function TabOneScreen({
                       image={doctor.image}
                       key={doctor.id}
                       location={doctor.location}
+                      id={doctor.id}
                     />
                   );
                 })
