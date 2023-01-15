96be3da52826235888e7a5d7121e2f434f2942cc
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-01-15 19:16:24 +0200

    new componnet

---
+++ components/CTABigWhite.tsx
@@ -0,0 +1,76 @@
+import {
+  StyleSheet,
+  TextStyle,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from 'react-native';
+import React from 'react';
+import { Feather, MaterialIcons } from '@expo/vector-icons';
+import { Text } from 'react-native-paper';
+import { darkGreen } from '../sharedStyles';
+
+type Props = {
+  icon: any;
+  text: string;
+  style?: ViewStyle;
+  onPress?: () => void;
+};
+
+const CTABigWhite = (props: Props) => {
+  return (
+    <View style={[styles.bookAppointment, props.style]}>
+      <TouchableOpacity
+        style={styles.bookAppointmentButton}
+        onPress={props.onPress}
+      >
+        {props.icon === 'calendar' ? (
+          <Feather name={props.icon} color={darkGreen} size={24} />
+        ) : (
+          <MaterialIcons name={props.icon} color={darkGreen} size={24} />
+        )}
+        <Text variant='headlineSmall' style={styles.bookAppointmentText}>
+          {props.text}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CTABigWhite;
+
+const styles = StyleSheet.create({
+  bookAppointment: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 50,
+    marginVertical: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 40,
+    width: '100%',
+  },
+  bookAppointmentText: {
+    color: darkGreen,
+    fontWeight: '600',
+    marginLeft: 12,
+  },
+  bookAppointmentButton: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: 2,
+    width: '100%',
+  },
+});
