a3bb495e1d250b290f82edb2b05bbdf552130ff2
sumaiadarra <44950315+sumaiadarra@users.noreply.github.com>
2023-03-04 14:54:36 +0100

    Upadate on privacy statment 4

--- navigation/ProfileStack.tsx
+++ navigation/ProfileStack.tsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import PrivacyStatmentScreen from '../screens/profile-patient-stack/PrivacyStatmentScreen';
 import PatientProfileScreen from '../screens/profile-patient-stack/PatientProfileScreen';
 import {ProfileStackParamList } from '../types';
 
@@ -8,6 +9,7 @@ export function PatientProfileNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name='PatientProfileScreen' component={PatientProfileScreen} />
+      <Stack.Screen name='PrivacyStatmentScreen' component={PrivacyStatmentScreen} />
     </Stack.Navigator>
   );
 }
