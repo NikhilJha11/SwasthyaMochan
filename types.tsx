3fd8c3cfdbee923f12182da4d69401141192267e
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-03-04 18:28:20 +0200

    adjusted param types

--- types.tsx
+++ types.tsx
@@ -30,11 +30,10 @@ export type RootStackScreenProps<Screen extends keyof RootStackParamList> =
   NativeStackScreenProps<RootStackParamList, Screen>;
 
 export type Doctor = {
-  image: string;
+  doctorId: number;
   name: string;
-  department: string;
-  location: string;
-  id?: number;
+  specialization: string;
+  locationId: number;
 };
 export type AppointmentConfirmationScreenParams = {
   doctor: string;
@@ -53,7 +52,6 @@ export type NotAuthStackParamList = {
   LoginScreen: undefined;
   RegisterScreen: undefined;
   OnboardingScreens: undefined;
-
 };
 export type RootTabParamList = {
   TabOne: undefined;
