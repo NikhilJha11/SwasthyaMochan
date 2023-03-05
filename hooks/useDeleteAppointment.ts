f701797aa66d7cbc573002b0db9b60605005951a
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-03-05 11:29:24 +0200

    changed imports

--- hooks/useDeleteAppointment.ts
+++ hooks/useDeleteAppointment.ts
@@ -1,15 +1,15 @@
-import axios from "axios";
-import { useMutation, useQuery } from "react-query";
-
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 export const useDeleteAppointment = async () => {
-    return useMutation({
-        mutationKey: ["delApp"],
-        mutationFn: async () => {
-        const res = axios.post(
-          "https://lachs.informatik.tu-chemnitz.de/planspiel/v1/deleteappointment/", {appointmentId:1,patientId:1,isPreliminaryCheckup:true}
-        );
-        return (await res).data;
-      },
-    });
-  }
+  return useMutation({
+    mutationKey: ['delApp'],
+    mutationFn: async () => {
+      const res = axios.post(
+        'https://lachs.informatik.tu-chemnitz.de/planspiel/v1/deleteappointment',
+        { appointmentId: 1, patientId: 1, isPreliminaryCheckup: true }
+      );
+      return (await res).data;
+    },
+  });
+};
