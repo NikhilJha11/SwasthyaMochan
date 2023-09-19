30ce0444c9e71bfff52d84abd28bf017774c2111
Palki Nanda <you@example.com>
2023-09-19 18:10:41 +0530

    commit

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:8000"), []);
+  const socket = useMemo(() => io("https://e-consultvc.onrender.com"), []);
 
   return (
     <SocketContext.Provider value={socket}>
